c7426fe066ffb438db324c1280c72535fc0337b0
fernandohelo <fernando1core@gmail.com>
2024-06-11 19:48:34 -0600

    Add Vercel's Web Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,4 @@
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
@@ -6,5 +7,9 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [react(), tailwind(), mdx()]
 });
